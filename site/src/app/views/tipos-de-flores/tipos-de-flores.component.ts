e46de5c3413b8ae628c46c53f9b7aa51c5e92cc8
Samara Santos <samara.souza2013@outlook.com>
2022-08-01 20:53:58 -0300

    tela tipos de flores

---
+++ site/src/app/views/tipos-de-flores/tipos-de-flores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipos-de-flores',
+  templateUrl: './tipos-de-flores.component.html',
+  styleUrls: ['./tipos-de-flores.component.css']
+})
+export class TiposDeFloresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
